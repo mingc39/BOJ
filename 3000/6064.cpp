165f7e63ac10871c6122130936bc0fd36f6bda92
mingc39 <43945033+mingc39@users.noreply.github.com>
2022-07-10 01:47:39 +0900

    Update 6064.cpp
    n과 y가 같은 경우 정답은 0이 아니나 0이 출력되는 경우가 있음

--- 3000/6064.cpp
+++ 3000/6064.cpp
@@ -21,24 +21,20 @@ int main() {
 		// k % n == y
 		// 를 만족하는 k 값을 찾아야한다
 		scanf("%d %d %d %d", &m, &n, &x, &y);
-		// 나머지 연산 결과의 범위는 0 ~ (n - 1)인데 x, y 는 1 ~ n 범위로 입력됨 
-		if(x == m) x = 0;
+		// 나머지 연산 결과의 범위는 0 ~ (n - 1)인데 y 는 1 ~ n 범위로 입력됨 
 		if(y == n) y = 0;
 		
-		int i, r = -1;
-		int l = lcm(n, m);
+		int i, l = lcm(n, m);
 		// i % m == x 을 만족하는 i은 x부터 m 간격으로 존재
 		// <m, n>(마지막 년도)는 m, n의 최소공배수이므로 m, n의 최소 공배수까지만 확인하면 됨 
-		for(i = x; i <= l; i += m) {
+		for(i = x; i <= l; i += m) 
 			// 해당 i가 i % n == y 만족하는지 확인 
-			if(i % n == y) {
-				r = i;
-				break;
-			}
-		}
+			if(i % n == y) break;
+		
 		// 조건에 맞는 값이 없을 수 있음
-		// 그런경우 { r = i; break; }가 수행되지 않아 r의 초기값 -1이 출력됨 
-		printf("%d\n", r);
+		// 그런경우 break; 로 반복문을 빠져나오지 않고 i > l 이 되어 반복문이 종료 됨 
+		if(i <= l) printf("%d\n", i);
+		else printf("%d\n", -1);
 	}
 	return 0;
 }
